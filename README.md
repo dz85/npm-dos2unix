bba21d5fd7142fe4c9319a2aaf94283ce3e579f1
David Z <geek@mrdz.org>
2023-04-04 17:38:02 +0800

    feat: 🎸 init

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ oclif example Hello World CLI
 # Usage
 <!-- usage -->
 ```sh-session
-$ npm install -g dos2unix
+$ npm install -g dos2unix.js
 $ dos2unix COMMAND
 running command...
 $ dos2unix (--version)
-dos2unix/0.0.1 darwin-arm64 node-v18.14.1
+dos2unix.js/0.0.1 darwin-arm64 node-v18.14.1
 $ dos2unix --help [COMMAND]
 USAGE
   $ dos2unix COMMAND
@@ -56,7 +56,7 @@ EXAMPLES
   $ dos2unix **/*.js
 ```
 
-_See code: [dist/index.ts](https://github.com/dz85/dos2unix/blob/v0.0.1/dist/index.ts)_
+_See code: [dist/index.ts](https://github.com/dz85/npm-dos2unix/blob/v0.0.1/dist/index.ts)_
 
 ## `dos2unix help [COMMANDS]`
 
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import {Command, Args} from '@oclif/core'
+import {globby} from 'globby'
+import fs from 'node:fs'
+import stream from 'node:stream'
+import {nanoid} from 'nanoid'
+import mmmagic from 'mmmagic'
+import AutoDetectDecoderStream  from 'autodetect-decoder-stream'
+
+class CRLF2LFTransform extends stream.Transform {
+  _transform(chunk: any, encoding: BufferEncoding, callback: stream.TransformCallback): void {
+    if (!Buffer.isBuffer(chunk))
+      return callback(new Error('CRLF2LFTransform needs buffers as its input.'))
+
+    this.push(chunk.toString().replace(/\r\n/g, '\n'))
+    callback()
+  }
+}
+
+export default class DefaultCommand extends Command {
+  static summary = 'A dos2UniX-like command-line tool written in Nodejs'
+
+  static examples = [
+    '$ <%= config.bin %> **/*.js',
+  ]
+
+  static flags = {
+    // 'ignore-file': Flags.file({description: 'specifies intentionally files to ignore', default: '.gitignore'}),
+    // dir: Flags.directory({description: 'the directory to be scanned', default: '.'}),
+    // file: Flags.file({description: 'the file to be scanned'}),
+  }
+
+  static args = {
+    glob: Args.string({description: 'the glob pattern to be scanned', default: '**/*'}),
+  }
+
+  static strict = false
+
+  async run(): Promise<void> {
+    const {args} = await this.parse(DefaultCommand)
+
+    const paths = await globby(args.glob, {gitignore: true})
+
+    const getMagicType = (path: string): Promise<{
+      mimeType?: string
+      encoding?: string
+    }> => new Promise((resolve, reject) => {
+      const magic = new mmmagic.Magic(mmmagic.MAGIC_MIME_ENCODING | mmmagic.MAGIC_MIME_TYPE)
+      magic.detectFile(path, (err, result) => {
+        if (err)
+          reject(err)
+        else if (typeof result === 'string') {
+          const results = result.split('; charset=')
+          this.log(path, result)
+          resolve({
+            mimeType: results[0],
+            encoding: results[1],
+          })
+        } else
+          resolve({})
+      })
+    })
+
+    await Promise.all(paths.map(async path => {
+      const stats = await fs.promises.stat(path)
+      if (stats.isFile()) {
+        const {mimeType, encoding} = await getMagicType(path)
+        if (typeof mimeType === 'string' && mimeType.startsWith('text/') && typeof encoding === 'string') {
+          const outputPath = `${path}.${nanoid(4)}`
+          // https://nodejs.org/zh-cn/docs/guides/backpressuring-in-streams
+          const input = fs.createReadStream(path)
+          const encodingTranslate = new AutoDetectDecoderStream()
+          const lfTranslate = new CRLF2LFTransform()
+          const output = fs.createWriteStream(outputPath)
+          await stream.promises.pipeline([input, encodingTranslate, lfTranslate, output])
+          await fs.promises.unlink(path)
+          await fs.promises.rename(outputPath, path)
+          this.log(`${path} 转化完成`)
+        }
+      }
+    }))
+  }
+}

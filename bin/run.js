9a333014ea24ec83f86108a873a166d89739b159
David Z <geek@mrdz.org>
2023-04-07 10:17:23 +0800

    refactor: remove oclif

--- bin/run.js
+++ bin/run.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-(async () => {
-  const oclif = await import('@oclif/core')
-  await oclif.execute({ type: 'esm', dir: import.meta.url })
-})()
+import { main } from '../dist/index.js'
+
+main()?.then()
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import buffer from 'node:buffer'
 import fs from 'node:fs'
 import stream from 'node:stream'
-import { Args, Command } from '@oclif/core'
+import { program } from 'commander'
 import { globby } from 'globby'
 import { nanoid } from 'nanoid'
 import { WASMagic } from 'wasmagic'
@@ -17,60 +17,48 @@ class CRLF2LFTransform extends stream.Transform {
   }
 }
 
-export default class DefaultCommand extends Command {
-  static summary = 'A dos2UniX-like command-line tool written in Nodejs'
+function main() {
+  program
+    .name('dos2unix.js')
+    .description('A dos2UniX-like command-line tool written in Nodejs')
+    .version('0.0.4')
+    .argument('[glob]', 'the glob pattern to be scanned', '**/*')
+    .action(async (glob) => {
+      const paths = await globby(glob as string, { gitignore: true })
 
-  static examples = [
-    '$ <%= config.bin %> **/*.js',
-  ]
+      const getMagicType = async (path: string): Promise<string> => {
+        const magic = await WASMagic.create()
+        const buf = await fs.promises.readFile(path)
+        return magic.getMime(buf)
+      }
 
-  static flags = {
-    // 'ignore-file': Flags.file({description: 'specifies intentionally files to ignore', default: '.gitignore'}),
-    // dir: Flags.directory({description: 'the directory to be scanned', default: '.'}),
-    // file: Flags.file({description: 'the file to be scanned'}),
-  }
-
-  static args = {
-    glob: Args.string({ description: 'the glob pattern to be scanned', default: '**/*' }),
-  }
-
-  static strict = false
-
-  async run(): Promise<void> {
-    const { args } = await this.parse(DefaultCommand)
-
-    const paths = await globby(args.glob, { gitignore: true })
-
-    const getMagicType = async (path: string): Promise<string> => {
-      const magic = await WASMagic.create()
-      const buf = await fs.promises.readFile(path)
-      return magic.getMime(buf)
-    }
-
-    const transformAndSave = async (path: string): Promise<void> => {
-      const outputPath = `${path}.${nanoid(4)}`
-      // https://nodejs.org/zh-cn/docs/guides/backpressuring-in-streams
-      const input = fs.createReadStream(path)
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      const encodingTranslate = (new AutoDetectDecoderStream()) as stream.Transform
-      const lfTranslate = new CRLF2LFTransform()
-      const output = fs.createWriteStream(outputPath)
-      await stream.promises.pipeline([input, encodingTranslate, lfTranslate, output])
-      /**
+      const transformAndSave = async (path: string): Promise<void> => {
+        const outputPath = `${path}.${nanoid(4)}`
+        // https://nodejs.org/zh-cn/docs/guides/backpressuring-in-streams
+        const input = fs.createReadStream(path)
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        const encodingTranslate = (new AutoDetectDecoderStream()) as stream.Transform
+        const lfTranslate = new CRLF2LFTransform()
+        const output = fs.createWriteStream(outputPath)
+        await stream.promises.pipeline([input, encodingTranslate, lfTranslate, output])
+        /**
        * linux下的rename是原子操作
        * @see https://man7.org/linux/man-pages/man2/rename.2.html#:~:text=will%20be%20atomically-,replaced,-%2C%20so%0A%20%20%20%20%20%20%20that%20there
        */
-      await fs.promises.rename(outputPath, path)
-      // this.log(`${path} 转化完成`)
-    }
-
-    await Promise.all(paths.map(async (path) => {
-      const stats = await fs.promises.stat(path)
-      if (stats.isFile()) {
-        const mimeType = await getMagicType(path)
-        if (typeof mimeType === 'string' && mimeType.startsWith('text/'))
-          await transformAndSave(path)
+        await fs.promises.rename(outputPath, path)
+        // this.log(`${path} 转化完成`)
       }
-    }))
-  }
+
+      await Promise.all(paths.map(async (path) => {
+        const stats = await fs.promises.stat(path)
+        if (stats.isFile()) {
+          const mimeType = await getMagicType(path)
+          if (typeof mimeType === 'string' && mimeType.startsWith('text/'))
+            await transformAndSave(path)
+        }
+      }))
+    })
+    .parse()
 }
+
+export { main }
